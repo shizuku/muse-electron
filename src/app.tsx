0d0b9ebfc335e81618589a451372b08573769972
shizuku <2112165916@qq.com>
2020-10-02 11:39:59 +0800

    add app.css

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { ipcRenderer } from "electron";
 import { MenuBar, MenuItem } from "./components/menu-bar";
 import { MuseNotation, Notation, MuseConfig } from "./components/muse-notation";
+import "./app.css";
 
 const App: React.FC = () => {
   let [notation, setNotation] = useState<Notation>();
@@ -20,7 +21,7 @@ const App: React.FC = () => {
     },
   ];
   return (
-    <div>
+    <div id="app">
       <header>
         <MenuBar item={menu} />
       </header>
@@ -31,7 +32,16 @@ const App: React.FC = () => {
       >
         hahah
       </button>
-      <div>{notation ? <MuseNotation notation={notation} /> : <></>}</div>
+      <button
+        onClick={() => {
+          console.log(notation?.code());
+        }}
+      >
+        log
+      </button>
+      <div className="notation">
+        {notation ? <MuseNotation notation={notation} /> : <></>}
+      </div>
     </div>
   );
 };
