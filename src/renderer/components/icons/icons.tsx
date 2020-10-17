80d24782a430fe934792c856fcd8705364e59598
shizuku <2112165916@qq.com>
2020-10-17 16:57:49 +0800

    refactor: change icon strokewidth

--- src/renderer/components/icons/icons.tsx
+++ src/renderer/components/icons/icons.tsx
@@ -10,7 +10,7 @@ export const LayoutHorizontalOutlinedSvg: FC = () => {
       height="1em"
       fill="none"
       stroke="currentColor"
-      strokeWidth={64}
+      strokeWidth={72}
     >
       <path d="M-100 120 H350 v656 H-100 Z" />
       <path d="M996 120 H546 v646 H996 Z" />
@@ -31,7 +31,7 @@ export const LayoutVerticalOutlinedSvg: FC = () => {
       height="1em"
       fill="none"
       stroke="currentColor"
-      strokeWidth={64}
+      strokeWidth={72}
     >
       <path d="M120 -100 V350 h656 V-100 Z" />
       <path d="M120 996 V546 h656 V996 Z" />
@@ -52,7 +52,7 @@ export const MaxmizeOutlinedSvg: FC = () => {
       height="1em"
       fill="none"
       stroke="currentColor"
-      strokeWidth={64}
+      strokeWidth={72}
     >
       <path d="M180 180 H716 V716 H180 V180 Z" />
     </svg>
@@ -72,7 +72,7 @@ export const MinimizeOutlinedSvg: FC = () => {
       height="1em"
       fill="none"
       stroke="currentColor"
-      strokeWidth={64}
+      strokeWidth={72}
     >
       <path d="M100 260 h536 v536 h-536 v-536 Z" />
       <path d="M260 260 V100 H796 V636 H636 " />
