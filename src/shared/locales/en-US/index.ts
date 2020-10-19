ea9dbb5c69cac84ed6922a6be8fc2a57b6917d8b
shizuku <2112165916@qq.com>
2020-10-19 14:06:29 +0800

    refactor: remove unused locale data

--- src/shared/locales/en-US/index.ts
+++ src/shared/locales/en-US/index.ts
@@ -1,11 +1,9 @@
-import { menu } from "./menu";
 import { welcome } from "./welcome";
 import { toolbar } from "./toolbar";
 import { header } from "./header";
 import { footer } from "./footer";
 
 export const enUS: Record<string, string> = {
-  ...menu,
   ...welcome,
   ...toolbar,
   ...header,
--- src/shared/locales/zh-CN/index.ts
+++ src/shared/locales/zh-CN/index.ts
@@ -1,11 +1,9 @@
-import { menu } from "./menu";
 import { welcome } from "./welcome";
 import { toolbar } from "./toolbar";
 import { header } from "./header";
 import { footer } from "./footer";
 
 export const zhCN: Record<string, string> = {
-  ...menu,
   ...welcome,
   ...toolbar,
   ...header,
